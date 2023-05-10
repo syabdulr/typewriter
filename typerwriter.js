0543755a3d33e172189718dead3c6343e563e521
@syabdulr <abdulsyed1998@gmail.com>
2023-05-09 21:15:40 -0400

    write a line appearing as an animation

--- typerwriter.js
+++ typerwriter.js
@@ -1,1 +1,13 @@
 const sentence = "hello there from lighthouse labs";
+let delay = 0;
+
+for (const char of sentence) {
+  setTimeout(() => {
+    process.stdout.write(char);
+  }, delay);
+  delay += 1000;
+}
+
+setTimeout(() => {
+  process.stdout.write('\n');
+}, delay);
